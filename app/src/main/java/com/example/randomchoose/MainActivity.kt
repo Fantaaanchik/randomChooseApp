d6d7b987c5e84af8b8d86b3f38dd2f27a4a018dd
Ibodullo <ibod.abdulloev65@gmail.com>
2022-03-23 09:55:53 +0500

    'First app commit'

---
+++ app/src/main/java/com/example/randomchoose/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.randomchoose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.randomchoose.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tvReto.setOnClickListener {
+            binding.button.text = getReto()
+        }
+    }
+    private fun getReto(): String{
+        return resources.getStringArray(R.array.retos)[randomNumber()]
+    }
+
+    private fun randomNumber(): Int {
+        val size = resources.getStringArray(R.array.retos).size - 1
+        return (0..size).random()
+    }
+}
